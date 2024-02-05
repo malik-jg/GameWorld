556b2b8573d51d7d7d60c6287355e35ed1535c08
Won Bean <malik_gg@gwu.edu>
2024-02-05 01:18:15 -0500

    edited test map, added custom properties to assets

--- assets/blocks.tsx
+++ assets/blocks.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="blocks" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="block.png" width="64" height="64"/>
+<tileset version="1.10" tiledversion="1.10.2" name="blocks" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="blockID" type="int" value="0"/>
+  </properties>
+  <image width="64" height="64" source="../../../../Downloads/summer_2023/Isometric-Game-master/assets/block.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="blockID" type="int" value="1"/>
+  </properties>
+  <image width="64" height="64" source="../../../../Downloads/summer_2023/Isometric-Game-master/assets/lava.png"/>
+ </tile>
 </tileset>
---
+++ assets/test_tiles.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="test_tiles" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="blockID" type="int" value="0"/>
+  </properties>
+  <image width="64" height="64" source="block.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="blockID" type="int" value="1"/>
+  </properties>
+  <image width="64" height="64" source="lava.png"/>
+ </tile>
+</tileset>
